9e78799deb85b153f842657bcd9b0a50b2e5f595
Tushar Kant <nanu.clickity@gmail.com>
2016-04-20 06:16:46 +0530

    watch for sass changes

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ var rename = require('gulp-rename');
 
 
 // Vars
-var sass_src = 'themes/atchai/static/src/sass/index.sass';
+var sass_src = 'themes/atchai/static/src/sass/';
 var sass_out = 'themes/atchai/static/dist/css/';
 var sass_options = {
 	file: 'index.min.css',
@@ -16,15 +16,18 @@ var sass_options = {
 
 
 // Tasks
-gulp.task('default', ['sass']);
+gulp.task('default', ['sass:watch']);
 gulp.task('sass:clean', function(cb){
 	return del([
 		sass_out + sass_options.file
 	], cb);
 });
 gulp.task('sass', ['sass:clean'], function(){
-	return gulp.src(sass_src)
-		.pipe(sass(sass_options).on('error', sass.logError))
+	return gulp.src(sass_src + 'index.sass')
+		.pipe(sass.sync(sass_options).on('error', sass.logError))
 		.pipe(rename(sass_options.file))
 		.pipe(gulp.dest(sass_out));
+});
+gulp.task('sass:watch', function(){
+	gulp.watch(sass_src + '**/*.sass' , ['sass']);
 });
